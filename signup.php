89b8b6467891811b34dd9a7ed297e32e2fd6f4b1
auocoder <ausekaruo17.comp@coep.ac.in>
2019-11-02 17:27:41 +0530

    welcome page and signup page added. About half of validation of signup done

---
+++ signup.php
@@ -0,0 +1,263 @@
+<?php
+	//declaring variables for error checking
+	$fnerr = " ";
+	$mnerr = " ";
+	$lnerr = " ";
+	$emailerr = " ";
+	$phoneerr = " ";
+	$cityerr = " ";
+	$countryerr = " ";
+	$addrerr = " ";
+	$cardNumerr = " ";
+	$dateerr = " ";
+	$rderr = " ";  //this is an error of the radio buttons for type of card
+	$uerr = " ";
+	$passerr = " ";
+	$numoferr = 0;
+	
+	$fname = " ";
+	$mname = " ";
+	$lname = " ";
+	$email = " ";
+	$phone = " ";
+	$city = " ";
+	$country = " ";
+	$addr = " ";
+	$cardNum = " ";
+	$dateofexp = " ";
+	$uname = " ";
+	$passw = " ";
+	if(isset($_POST['submit'])){
+		$fname = $_POST['fname'];
+		if(empty($_POST['fname'])){
+			$fnerr = "First Name cannot be left empty!";
+			$numoferr++;
+		}else{
+			if(!preg_match('/^[a-zA-Z]*$/',$fname)){
+				$fnerr = "Please enter valid first name consisting of only alphabets";
+				$numoferr++;
+			}
+		}
+		//validated first name
+		
+		$mname = $_POST['mname'];
+		if(empty($_POST['mname'])){
+			$fnerr = "Middle Name cannot be left empty!";
+			$numoferr++;
+		}else{
+			if(!preg_match('/^[a-zA-Z]*$/',$mname)){
+				$fnerr = "Please enter valid middle name consisting of only alphabets";
+				$numoferr++;
+			}
+		}
+		//validated middle name
+		
+		$lname = $_POST['lname'];
+		if(empty($_POST['lname'])){
+			$fnerr = "Last Name cannot be left empty!";
+			$numoferr++;
+		}else{
+			if(!preg_match('/^[a-zA-Z]*$/',$lname)){
+				$fnerr = "Please enter valid last name consisting of only alphabets";
+				$numoferr++;
+			}
+		}
+		//last name validated
+		
+		$email = $_POST['email'];
+		if(empty($_POST['email'])){
+			$emailerr = "Please enter your email";
+			$numoferr++;
+		}else{
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$emailerr = "Please enter valid email.";
+				$numoferr++;
+			}
+		}
+		//email validated
+		
+		$phone = $_POST['phone'];
+		$city = $_POST['city'];
+		$country = $_POST['country'];
+		$addr = $_POST['add'];
+		$cardNum = $_POST['card'];
+		$dateofexp = $_POST['dateofexp'];
+		
+		if(isset($_POST['typeofcard']) && $_POST['typeofcard'] == "credit")
+			$typeofcard = "credit";
+		else if(isset($_POST['typeofcard']) && $_POST['typeofcard'] == "debit")
+			$typeofcard = "debit";
+		else
+			$rderr = "You must select a type of card!";
+	}
+?>
+
+<html>
+	<head>
+		<title>SignUp</title>
+		<style>
+		
+			* {
+			  box-sizing: border-box;
+			}
+
+			.row{
+				text-align: center;
+			}
+			.column {
+			  float: left;
+			  padding: 10px;
+			  height: 230%; /* Should be removed. Only for demonstration */
+			}
+
+			.left, .right {
+			  width: 15%;
+			}
+
+			.middle {
+			  width: 70%;
+			  
+			}
+
+			/* Clear floats after the columns */
+			.row:after {
+			  content: "";
+			  display: table;
+			  clear: both;
+			}
+			
+			.error{ 
+				color:red;
+			}
+			
+			body{
+  				 
+  				text-align: center;
+  				font-family: "Comic Sans MS";
+  	
+			}
+			
+			h1{
+				
+				text-align: center;
+			}
+			fieldset{
+				background-color: #808080;
+				margin-left: auto;
+  				margin-right: auto;
+  				width : 550px;
+  				border-style: solid;
+				border-width: 2px;
+				border-radius: 8px;
+				overflow: auto;
+				/*margin-left: 300px;
+				margin-right: 300px;*/
+			}
+			legend{
+				font-size: 20px 
+			}
+			p{
+				border-style: hidden;
+  				/*margin-left: 80px;
+				margin-right: 80px;*/
+				background-color: #aaa;
+				padding-top: 4px;
+				padding-right: 10px;
+				padding-bottom: 4px;
+				padding-left: 10px;
+			}
+			textarea{
+				
+				font-size: 15px
+			}
+
+
+		</style>
+	</head>
+	<body>
+		
+		
+
+		<div class="row">
+		  <div class="column left" style="background-color: #000000;">
+		    
+		    
+		  </div>
+		  <div class="column middle" style="background-color:#ccc;">
+		  	<h1>SIGN UP</h1>
+		  	<br><br><span class = "error">ALL details are mandatory. </span> 
+			<form method = "POST">
+			<br>
+				<h3>Personal Details</h3>
+				<fieldset>
+				
+					First Name : <input type = "text" name = "fname" value ='<?php echo htmlentities($fname)?>'>
+					<span class = "error"> <?php echo $fnerr; ?></span> <br><br>
+					
+					Middle Name : <input type = "text" name = "mname" value ='<?php echo htmlentities($mname)?>'> 
+					<span class = "error"> <?php echo $mnerr; ?> </span><br><br>
+					
+					Lase Name : <input type = "text" name = "lname" value ='<?php echo htmlentities($lname)?>'> 
+					<span class = "error"> <?php echo $lnerr; ?> </span><br><br>
+					
+					Email Address : <input type = "text" name = "email" value ='<?php echo htmlentities($email)?>'> 
+					<span class = "error"> <?php echo $emailerr; ?></span><br><br>
+					
+					Phone Number : <input type = "text" name = "phone" value ='<?php echo htmlentities($phone)?>'>
+					<span class = "error"> <?php echo $phoneerr; ?></span> <br><br>
+					
+					Country : <input type = "text" name = "country" value ='<?php echo htmlentities($country)?>'> 
+					<span class = "error"> <?php echo $countryerr; ?></span><br><br>
+					
+					City : <input type = "text" name = "city" value ='<?php echo htmlentities($city)?>'> 
+					<span class = "error"> <?php echo $cityerr; ?> </span><br><br>
+					
+					Address : <br>
+					<textarea name = "add" cols = "50" rows = "4" value ='<?php echo htmlentities($addr)?>' placeholder = "Input your address here"> </textarea>
+					<span class = "error"> <?php echo $addrerr; ?></span><br><br>
+					
+				</fieldset>
+				<br><br>
+				<h3>Payment Details</h3>
+				<fieldset>
+				
+				
+					Credit or Debit Card Number : <input type = "text" name = "card" value ='<?php echo htmlentities($cardNum)?>'> 
+					<span class = "error"> <?php echo $cardNumerr; ?></span><br><br>
+					
+					Date of Expiry : <input type = "date" name = "dateofexp" value ='<?php echo htmlentities($dateofexp)?>'> 
+					<span class = "error"> <?php echo $dateerr; ?></span><br><br>
+					
+					Type of Card : <br><br>
+					Credit Card <input type = "radio" name = "typeofcard" value = "credit"><br>
+					Debit Card <input type = "radio" name = "typeofcard" value = "debit"><br><br>
+				</fieldset>
+				<br><br>
+				<input type = "submit" name = "submit" value = "Sign Up">
+				<h3>Login Id and Password</h3>
+				<fieldset>
+					Username : <input type = "text" name = "uname" value ='<?php echo htmlentities($uname)?>'> 
+					<span class = "error"> <?php echo $uerr; ?></span><br><br>
+					
+					Password : <input type = "password" name = "passkey" value ='<?php echo htmlentities($passw)?>'> 
+					<span class = "error"> <?php echo $passerr; ?></span><br><br>
+				</fieldset>
+			</form>
+		   	
+		  </div>
+		  <div class="column right" style="background-color:#000000;">
+		    
+		   
+		  </div>
+		</div>
+		
+		<p>
+			<b><u>Airline Reservation System</u>
+			made by
+			Urvee Ausekar  111713007 and 
+			Tejaswini Kokate  111703075
+			</b>
+		</p>
+	
+	</body>
+</html>
---
+++ welcomepage.php
@@ -0,0 +1,127 @@
+<?php
+
+?>
+
+<html>
+	<head>
+		<title>AirlineReservationSystem</title>
+		<style>
+		
+		
+			* {
+			  box-sizing: border-box;
+			}
+
+			.row{
+				text-align: center;
+			}
+			.column {
+			  float: left;
+			  padding: 10px;
+			  height: 100%; /* Should be removed. Only for demonstration */
+			}
+
+			.left, .right {
+			  width: 15%;
+			}
+
+			.middle {
+			  width: 70%;
+			  
+			}
+
+			/* Clear floats after the columns */
+			.row:after {
+			  content: "";
+			  display: table;
+			  clear: both;
+			}
+			
+			a{
+			
+			}
+			body{
+  				 
+  				text-align: center;
+  				font-family: "Comic Sans MS";
+  	
+			}
+			
+			h1{
+				
+				text-align: center;
+			}
+			fieldset{
+				background-color: #808080;
+				margin-left: auto;
+  				margin-right: auto;
+  				width : 200px;
+  				border-style: solid;
+				border-width: 2px;
+				border-radius: 8px;
+				/*margin-left: 300px;
+				margin-right: 300px;*/
+			}
+			legend{
+				font-size: 20px 
+			}
+			p{
+				border-style: hidden;
+  				/*margin-left: 80px;
+				margin-right: 80px;*/
+				background-color: #aaa;
+				padding-top: 4px;
+				padding-right: 10px;
+				padding-bottom: 4px;
+				padding-left: 10px;
+			}
+			textarea{
+				
+				font-weight:bold;
+				font-size: 20px
+			}
+
+
+		</style>
+	</head>
+	<body>
+		
+		
+
+		<div class="row">
+		  <div class="column left" style="background-color: #000000;">
+		    
+		    
+		  </div>
+		  <div class="column middle" style="background-color:#ccc;">
+		  	<h1>WELCOME TO YOUR AIRLINE RESERVATIONS PROVIDER!!</h1>
+			New user? Then <a href = "/var/www/html/myfiles/DBMSmp/signup.php">sign up!</a><br><br>
+			<b>Or Sign In</b><br>
+			<fieldset>
+				<legend></legend>
+				LoginId <input type = "text" name = "loginid "><br><br>
+				Password <input type = "password" name = "passkey" ><br><br>
+				<input type = "submit" name = "submit" value = "Log In"><br><br>
+			</fieldset>
+			<br><br><br><br>
+			<a href = "/var/www/html/myfiles/A2/subnetCalcClassful.php">BROWSE FLIGHTS</a>
+			<br><br><br><br>
+			
+		   
+		  </div>
+		  <div class="column right" style="background-color:#000000;">
+		    
+		   
+		  </div>
+		</div>
+		
+		<p>
+			<b><u>Airline Reservation System</u>
+			made by
+			Urvee Ausekar  111713007 and 
+			Tejaswini Kokate  111703075
+			</b>
+		</p>
+	
+	</body>
+</html>
