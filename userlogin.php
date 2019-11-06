cf1c95debc9260e8bd645ed7bef79adf0a782097
auocoder <ausekaruo17.comp@coep.ac.in>
2019-11-06 12:53:30 +0530

    userlogin.php is the user homepage that user will be redirected to upon login

---
+++ userlogin.php
@@ -0,0 +1,124 @@
+<?php
+	$userid = " ";  //FIXME: add real userid here
+	if(isset($_POST['submit'])){
+		//get details of flight booked by from database store them in variables
+	}
+?>
+
+<html>
+	<head>
+		<title>AirlineReservationSystem</title>
+		<style>
+		
+		
+			* {
+			  box-sizing: border-box;
+			}
+
+			.row{
+				text-align: center;
+			}
+			.column {
+			  float: left;
+			  padding: 10px;
+			  height: 100%; 
+			}
+
+			.left, .right {
+			  width: 20%;
+			}
+
+			.middle {
+			  width: 60%;
+			  
+			}
+
+			/* Clear floats after the columns */
+			.row:after {
+			  content: "";
+			  display: table;
+			  clear: both;
+			}
+			
+			a{
+			
+			}
+			body{
+  				 
+  				text-align: center;
+  				font-family: "Comic Sans MS";
+  	
+			}
+			
+			h1{
+				
+				text-align: center;
+			}
+			fieldset{
+				background-color: #808080;
+				margin-left: auto;
+  				margin-right: auto;
+  				width : 200px;
+  				border-style: solid;
+				border-width: 2px;
+				border-radius: 8px;
+				/*margin-left: 300px;
+				margin-right: 300px;*/
+			}
+			legend{
+				font-size: 20px 
+			}
+			p{
+				border-style: hidden;
+  				/*margin-left: 80px;
+				margin-right: 80px;*/
+				background-color: #aaa;
+				padding-top: 4px;
+				padding-right: 10px;
+				padding-bottom: 4px;
+				padding-left: 10px;
+			}
+			textarea{
+				
+				font-weight:bold;
+				font-size: 20px
+			}
+
+
+		</style>
+	</head>
+	<body>
+		
+		
+
+		<div class="row">
+		  <div class="column left" style="background-color: #000000;">
+		    
+		    
+		  </div>
+		  <div class="column middle" style="background-color:#ccc;">
+		  	<h1>HELLO @<?php echo $userid;?></h1>
+				<br><br>
+				<!--user_id, plane_id, date, src, dest, src_country, dest_country, dept_time)-->
+				<a href = "browseflights.php">Browse Flights</a><br><br><br><br>
+				<form method = "POST">
+					<input type = "submit" name = "submit" value = "View booked flights">
+				</form>
+				<?php //echo details of booked flights here  ?>
+		  </div>
+		  <div class="column right" style="background-color:#000000;">
+		    
+		   
+		  </div>
+		</div>
+		
+		<p>
+			<b><u>Airline Reservation System</u>
+			made by
+			Urvee Ausekar  111713007 and 
+			Tejaswini Kokate  111703075
+			</b>
+		</p>
+	
+	</body>
+</html>
